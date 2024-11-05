3f6ff6d25f2d044cb46e873422e7a80a017ea1f2
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-11-05 13:08:50 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (32.31%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,5 +1,31 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+                # The number of unique paths can be seen as the number of ways to choose m−1 downs and n−1 rights, regardless of the order. In combinatorial terms. 
+            
+            ways_to_choose_n_1 = math.comb(m+n-2, n-1)
+            
+            return ways_to_choose_n_1
+        # math solution
+        # [[][][]].  # all the outer layers came through 1 path only, and max they are part of m-1 + n-1 paths, the inner layers could have been discovered through 2 different ways, either coming from up or from the left, 
+        # 7 x 3 => 28    2 x 2 => 2.   2 x 3 => 3   (1 + 2) 
+        # [[][][]]
+
+
+
+        # OBS: every time we make right or down move, we decrease either m or n by 1 and create a new subproblem
+        # Q: how many different such subproblems exist (mn)
+
+        
+        # [[][][][]]
+        # [[][][][]]
+        # [[][][][]]
+
+
+
+
+# DP solution
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         
         grid = [[0 for i in range(n)] for j in range(m)]
         print(grid)
