e76c8337ab4e714a75e362a708a792e04298c010
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-11-06 11:16:42 +0300

    Time: 189 ms (61.11%) | Memory: 21.1 MB (29.31%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        idx = 0
+        start, end = ListNode(), ListNode()
+        ptr = list1
+        while ptr:
+            if idx == a - 1: 
+                start = ptr
+            if idx == b + 1: 
+                end = ptr
+            idx += 1
+            ptr = ptr.next
+
+        start.next = list2
+        while start: 
+            last = start
+            start = start.next
+        last.next = end
+
+        return list1
