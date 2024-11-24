b8085e0b97fd3289496b5518eb49e4cfc5b1edfe
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-11-25 00:37:52 +0300

    Time: 0 ms (100.00%) | Memory: 16.8 MB (6.57%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,11 +1,15 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        mask = 0xFFFFFFFF
-        max_int = 0x7FFFFFFF
-
-        while b != 0:
-            carry = (a & b) << 1
-            a = (a ^ b) & mask
-            b = carry & mask
-
-        return a if a <= max_int else ~(a ^ mask)
+        
+        # 32 bit mask in hexadecimal
+        mask = 0xffffffff
+        
+        # works both as while loop and single value check 
+        while (b & mask) > 0:
+            
+            carry = ( a & b ) << 1
+            a = (a ^ b) 
+            b = carry
+        
+        # handles overflow
+        return (a & mask) if b > 0 else a
