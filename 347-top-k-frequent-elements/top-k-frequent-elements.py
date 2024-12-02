b8415f5d4844cfd355dc4c150f8c2b43ba4ed036
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-12-02 12:48:03 +0300

    Time: 12 ms (15.78%) | Memory: 20.7 MB (48.26%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,22 @@
 
-# class Solution:
-#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         count = {}
-#         bucket = [[] for i in range(len(nums) + 1)]
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        bucket = [[] for i in range(len(nums) + 1)]
 
-#         for num in nums: 
-#             count[num] = count.get(num, 0) + 1
-#         for num, freq in count.items(): 
-#             bucket[freq].append(num)
+        for num in nums: 
+            count[num] = count.get(num, 0) + 1
+        for num, freq in count.items(): 
+            bucket[freq].append(num)
         
-#         res = []
-#         print(bucket)
-#         for i in range(len(nums), 0, -1): 
+        res = []
+        print(bucket)
+        for i in range(len(nums), 0, -1): 
             
-#             for num in bucket[i]:
-#                 res.append(num)
-#                 if len(res) == k:
-#                     return res
+            for num in bucket[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
 
     # Bucket sort: O(n) time and O(n) space complexity
     # Max Heap: O(n + klogn) time and O(n) space complexity      
