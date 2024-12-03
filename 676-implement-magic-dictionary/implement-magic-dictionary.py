a5ce82d3bd1fa0eecb26bbdf159686882e2cc843
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-12-03 09:48:05 +0300

    Time: 112 ms (51.79%) | Memory: 20 MB (17.64%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.py
+++ 676-implement-magic-dictionary/implement-magic-dictionary.py
@@ -1,36 +1,3 @@
-# class TrieNode:
-
-#     def __init__(self):
-#         self.children = collections.defaultdict(TrieNode)
-#         self.is_end = False
-
-
-# class MagicDictionary:
-
-#     def __init__(self):
-#         self.trie = TrieNode()
-        
-
-#     def buildDict(self, dictionary: List[str]) -> None:
-#         for word in dictionary:
-#             cur = self.trie
-#             for ch in word:
-#                 cur = cur.children[ch]
-#             cur.is_end = True
-        
-
-#     def search(self, searchWord: str) -> bool:
-#         n = len(searchWord)
-#         def dfs(root: TrieNode, cnt: int, i: int) -> bool:
-#             if cnt > 1 or i == n: return cnt == 1 and root.is_end
-#             return any([dfs(root.children[c], cnt + int(c != searchWord[i]), i + 1) for c in root.children])
-        
-#         return dfs(self.trie, 0, 0)
-# Your MagicDictionary object will be instantiated and called as such:
-# obj = MagicDictionary()
-# obj.buildDict(dictionary)
-# param_2 = obj.search(searchWord)
-
 
 # HashSet solution
 
