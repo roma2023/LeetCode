e31b6946dcb430139b6c63c21ccc919d1662397d
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-12-03 13:08:01 +0300

    Time: 18 ms (39.91%) | Memory: 18.1 MB (5.52%) - LeetSync

--- 1775-design-an-ordered-stream/design-an-ordered-stream.py
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.py
@@ -1,13 +1,13 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.data = [None]*n
-        self.ptr = 0 # 0-indexed 
+        self.ptr = 1
+        self.stream = [None] * (n+2)
 
     def insert(self, id: int, value: str) -> List[str]:
-        id -= 1 # 0-indexed 
-        self.data[id] = value 
-        if id > self.ptr: return [] # not reaching ptr 
-        
-        while self.ptr < len(self.data) and self.data[self.ptr]: self.ptr += 1 # update self.ptr 
-        return self.data[id:self.ptr]
+        self.stream[id] = value
+        if id == self.ptr:
+            while self.stream[self.ptr] is not None:
+                self.ptr += 1
+            return self.stream[id:self.ptr]
+        return []
