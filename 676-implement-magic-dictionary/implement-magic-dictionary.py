c18688a3b2e54917e30db3d555061c22b8b8e485
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-12-03 09:46:40 +0300

    Time: 309 ms (13.50%) | Memory: 20.6 MB (9.22%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.py
+++ 676-implement-magic-dictionary/implement-magic-dictionary.py
@@ -31,36 +31,3 @@ class MagicDictionary:
 # obj.buildDict(dictionary)
 # param_2 = obj.search(searchWord)
 
-
-# HashSet solution
-
-'''
-class MagicDictionary:
-    def __init__(self):
-        self.dict = {}
-
-    def buildDict(self, dictionary: List[str]) -> None:
-        for word in dictionary:
-            for i in range(len(word)):
-                replaced = word[:i] + '*' + word[i+1:]
-                if replaced in self.dict:
-                    self.dict[replaced].append(word)
-                else:
-                    self.dict[replaced] = [word]
-
-    def search(self, searchWord: str) -> bool:
-        for i in range(len(searchWord)):
-            replaced = searchWord[:i] + '*' + searchWord[i+1:]
-            if replaced in self.dict:
-                words = self.dict[replaced]
-                for word in words:
-                    if word != searchWord and len(word) == len(searchWord):
-                        match = True
-                        for j in range(len(searchWord)):
-                            if searchWord[j] != word[j] and replaced[j] != '*':
-                                match = False
-                                break
-                        if match:
-                            return True
-        return False
-'''
