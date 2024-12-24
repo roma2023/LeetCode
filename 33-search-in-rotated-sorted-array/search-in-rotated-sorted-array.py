cfeb550a78c065c5797436917c56ae580b2e58e8
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2024-12-24 22:21:06 +0500

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.03%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -22,12 +22,12 @@ class Solution:
 
             if nums[l] > nums[m]:
                 if target < nums[m] or target >= nums[l]:
-                    r = m
+                    r = m - 1
                 else: 
                     l = m + 1
             else:
                 if target < nums[m] and target >= nums[l]: 
-                    r = m
+                    r = m - 1
                 else: 
                     l = m + 1
         print(l,m,r)
