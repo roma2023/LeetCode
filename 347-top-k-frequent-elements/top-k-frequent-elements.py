a65e31ec7c1ad6c142989a9f2a9b4e88ecc21fb2
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2025-10-11 23:28:50 -0400

    Time: 7 ms (50.69%) | Memory: 21.2 MB (80.86%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,57 +1,25 @@
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        bucket = [[] for i in range(len(nums) + 1)]
+        # so we have to create a freq map
+        freqMap = {} # num: freq
 
-        for num in nums: 
-            count[num] = count.get(num, 0) + 1
-        for num, freq in count.items(): 
-            bucket[freq].append(num)
+        for n in nums: 
+            freqMap[n] = freqMap.get(n, 0) + 1
         
-        res = []
-        print(bucket)
-        for i in range(len(nums), 0, -1): 
-            
-            for num in bucket[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+        # create a list of num, freq pairs
+        freqPairs = []
+        for num, freq in freqMap.items(): 
+            freqPairs.append([num, freq])
 
-    # Bucket sort: O(n) time and O(n) space complexity
-    # Max Heap: O(n + klogn) time and O(n) space complexity      
-    # Sorting: O(nlogn) time and O(n) space complexity
-    # Counter in Python: O(n + klogn) time and O(n + k) space complexity 
-    
-    #        counter = Counter(nums)
-    #        return [num for num, _ in counter.most_common(k)]
+        print(freqPairs)
 
-    # Quick Select Algo O(n)
+        # sort the freq Pairs in desc order
+        freqPairs = sorted(freqPairs, reverse=True, key=lambda pair: pair[1])
 
-##
+        print(freqPairs)
 
-from collections import Counter
-import random
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        freq_tab = list(count.values())   
         
-        def quickSelect(nums, k):
-            pivot = random.choice(nums)
-            left, right = [], []
-            for num in nums:
-                if num >= pivot:
-                    left.append(num)
-                else:
-                    right.append(num)
-            
-            if len(left) == k:
-                return left
-            elif len(left) > k:
-                return quickSelect(left, k)
-            else:
-                return quickSelect(right, k - len(left)) + left
-        
-        index = quickSelect(freq_tab, k)
-        return [key for key, val in count.items() if val in index]
+        # return top k
+        topK = [freqPairs[i][0] for i in range(k)]
+
+        return topK
