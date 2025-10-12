bf6a0e56401ade772435a1510620c5766bc24e75
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2025-10-11 23:45:38 -0400

    Time: 15 ms (10.14%) | Memory: 24.3 MB (13.65%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,25 +1,32 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # so we have to create a freq map
-        freqMap = {} # num: freq
+        # use algo bucketsort, where we will create a freq list of size n + 1
+        # where each index will represent a freq, then when we traverse the freqmap 
+        # we can store the numbers at idx = freq
+        # afterwards we will through the bucket right to left to retrieve k elements
+
+        freqmap = {}
 
         for n in nums: 
-            freqMap[n] = freqMap.get(n, 0) + 1
+            freqmap[n] = freqmap.get(n, 0) + 1
+
+        bucket = [[] for i in range(len(nums)+1)]
+        for num, freq in freqmap.items():
+            bucket[freq].append(num)
+
+
+        result = []
+
+        for i in range(len(nums), 0, -1):
+            b = bucket[i]
+            if len(b) > k: 
+                result = result + b[:k]
+                return result
+            
+            result = result + b
+            k -= len(b)
         
-        # create a list of num, freq pairs
-        freqPairs = []
-        for num, freq in freqMap.items(): 
-            freqPairs.append([num, freq])
-
-        print(freqPairs)
-
-        # sort the freq Pairs in desc order
-        freqPairs = sorted(freqPairs, reverse=True, key=lambda pair: pair[1])
-
-        print(freqPairs)
-
+        return result
         
-        # return top k
-        topK = [freqPairs[i][0] for i in range(k)]
-
-        return topK
+# TC => O(n)
+# SC => O(n)
