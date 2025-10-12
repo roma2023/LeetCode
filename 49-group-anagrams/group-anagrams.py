f6afca53813ea2dc4dede4a532e85e6a8bc130e7
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2025-10-11 23:17:06 -0400

    Time: 22 ms (20.97%) | Memory: 22.7 MB (11.83%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,25 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            res[tuple(count)].append(s)
-        return list(res.values())
+        # cretae  ahasMap of all, then store the indices in the hashMap of hashmaps mapped to list of indices
+        A = dict()
+        for s in strs: 
+            H = self.createHash(s)
+            A[H] = A.get(H, [])
+            A[H].append(s)
+
+        # combine groups into a list
+        results = []
+        for h, g in A.items(): 
+            results.append(g)
+        return results
+        
+    def createHash(self, s: str) -> dict(): 
+        count = [0] * 26
+        for char in s:
+            count[ord(char) - ord('a')] += 1
+
+        return tuple(count)
+
 # Complexity 
 # TC => O(m * nlogn) m is the numnber of strings, n is the length of the longest string
 # SC => O(m * n) for new strings mn and hashmap mn
