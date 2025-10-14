02a19db995bd7301f876de6872b873779e30ff3c
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2025-10-13 20:00:27 -0400

    Time: 3 ms (8.05%) | Memory: 17.7 MB (82.33%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,8 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
-        dp = nums + [0]
-        # [1,2,3,1] => [1,2,3,1,0]
-        for i in range(len(dp) - 3, -1, -1): 
-            dp[i] = max(dp[i+1], dp[i] + dp[i+2])
-        return dp[0]
+        # ok, let's solve the sub problem which is max amount of money we can rob from 1 house nghbh
+        # [1,2,3,1]
+        # [1] => max = 1, [3,1] => max(3,1), 
+        # [2,3,1] => you either take 2 + dp[0] or dp[1]
+
+        dp = [0] * (len(nums) + 2) 
+
+        for i in range(len(nums) - 1, -1, -1): 
+            dp[i] = max(dp[i + 2] + nums[i], dp[i+1])
+
+        return max(dp)
