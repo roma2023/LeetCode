7a38e73b54e65ab46a5eb09be26e295036d7c38c
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2025-10-27 17:56:23 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.84%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,21 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        # brute force => just multiply by n in a loop of O(n)
+        # optimized => divide and conquer, logn 
+        if x == 0:
+            return 0 
+
+        def rec(x, n):
+            if n == 0:
+                return 1
+            if x == 0:
+                return 0 
+
+            res = rec(x, n // 2)
+            res = res * res
+            return x * res if n%2 else res
+        
+        res = rec(x, abs(n))
+        return res if n>=0 else 1/res
+
+            
