8b3e8ceb055f7c25e001ed902e9e2a527fd4b93d
Raman Saparkhan <109868980+roma2023@users.noreply.github.com>
2025-10-27 17:24:24 -0400

    Time: 8075 ms (5.00%) | Memory: 18.1 MB (21.52%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,24 +1,34 @@
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # 1-15 => length for this certainlength in how many ways we can construct a word, 2^mn * 15
+        
         ROWS, COLS = len(board), len(board[0])
+        visited = set() # (r, c)
+        length = len(word)
 
-        def dfs(r, c, i):
-            if i == len(word):
+        def dfs(row, col, i):
+
+            if i == length:
                 return True
-            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or
-                word[i] != board[r][c] or board[r][c] == '#'):
+                
+            if row not in range(0, ROWS) or col not in range(0, COLS) or (row, col) in visited or word[i] != board[row][col]:
                 return False
-
-            board[r][c] = '#'
-            res = (dfs(r + 1, c, i + 1) or
-                   dfs(r - 1, c, i + 1) or
-                   dfs(r, c + 1, i + 1) or
-                   dfs(r, c - 1, i + 1))
-            board[r][c] = word[i]
+            
+        
+            visited.add((row, col))
+            res = (dfs(row+1, col, i+1) or
+                    dfs(row, col+1, i+1) or
+                    dfs(row-1, col, i+1) or
+                    dfs(row, col-1, i+1))
+            visited.remove((row, col))
             return res
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == word[0]:  
+                    if dfs(row, col, 0):
+                        return True
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0):
-                    return True
-        return False
+        return False 
+
